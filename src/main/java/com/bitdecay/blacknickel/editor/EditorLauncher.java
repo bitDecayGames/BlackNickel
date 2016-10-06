ac8b074ac6bd19e7e06ab473cb59fd1e69f85fe6
mwingfield <mwingfie@gmail.com>
2016-10-06 08:09:58 -0600

    added code to level editor launcher to make it easier to load/save levels. (start in the levels dir)

--- src/main/java/com/bitdecay/blacknickel/editor/EditorLauncher.java
+++ src/main/java/com/bitdecay/blacknickel/editor/EditorLauncher.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.bitdecay.blacknickel.Launcher;
 import com.bitdecay.blacknickel.util.TexturePackerUtils;
 import com.bitdecay.jump.leveleditor.render.LevelEditor;
+import org.apache.log4j.BasicConfigurator;
 
 /**
  * Launches the editor around the game
@@ -13,6 +14,8 @@ import com.bitdecay.jump.leveleditor.render.LevelEditor;
 public class EditorLauncher {
 
     public static void main(String[] arg) {
+        BasicConfigurator.configure();
+
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = Launcher.conf.getInt("resolution.editor.width");
         config.height = Launcher.conf.getInt("resolution.editor.height");
@@ -21,7 +24,9 @@ public class EditorLauncher {
 
         TexturePackerUtils.pack();
 
+        LevelEditor.setFileDialogStartingDirectory("src/main/resources/level");
         LevelEditor.setAssetsFolder("../jump/jump-leveleditor/assets");
+
         new LwjglApplication(new EditorApp(), config);
     }
 }
